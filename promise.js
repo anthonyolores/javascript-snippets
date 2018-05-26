42fd50f2a9f01b7b3d461b2f0b278ad16a4255c1
anthonyolores <tonyoolores@gmail.com>
2018-05-26 17:20:43 +1200

    added promise.js

---
+++ promise.js
@@ -0,0 +1,15 @@
+const promiseFunc = (b) => {
+    return new Promise((resolve, reject) => {
+      return b? resolve("promise resolved") : reject("promise rejected");
+    });
+};
+ 
+//pass true to resolve otherwise false to reject promise
+promiseFunc(true)
+.then((msg) => {
+  console.log(msg)
+})
+.catch((msg) => {
+  console.log(msg)
+});
+
